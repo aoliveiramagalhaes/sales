b98bd75a8d419399d8a4b655d5509e5c42b1659d
Aline <aoliveiramagalhaes@gmail.com>
2020-06-01 20:44:49 -0400

    Add sample HTML

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express()
 const path = require('path');
 const port = 3000
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
