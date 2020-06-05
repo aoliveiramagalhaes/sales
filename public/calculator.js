7706d6a8286dee54b26562d639f0efa8a97ce6cb
Aline <aoliveiramagalhaes@gmail.com>
2020-06-04 21:04:59 -0400

    add my notes

--- public/calculator.js
+++ public/calculator.js
@@ -15,19 +15,23 @@ function divide(a, b) {
 }
 // "a" + "b" => "ab"
 // 1 + 1 => 2
+// connect the elements in html with JS.
 
 const buttonSum = document.getElementById("button-sum")
 const buttonSubtract = document.getElementById("button-subtract")
 const buttonMultiply = document.getElementById("button-multiply")
 const buttonDivide = document.getElementById("button-divide")
 
+// eventListener: what the button needs to do. In this case, the user needs to click on the /n
+// button to make it work.
 buttonSum.addEventListener("click", function(event) {
     const inputA = document.getElementById("input-a")
     const inputB = document.getElementById("input-b")   
     const inputResult = document.getElementById("input-result")
 
 
-    // int => Integer = Number = 1, 1000, 1000000
+    // int => Integer = Number = 1, 1000, 1000000. No decimals.
+    // parseInt: to let JS know that it's a number, so JS can do the math.
     const valueA = parseInt(inputA.value)
     const valueB = parseInt(inputB.value)
 
