445de7b0b3e02c7d8961ff76c95ce597724f9935
Aline <aoliveiramagalhaes@gmail.com>
2020-06-04 20:42:19 -0400

    add multiplication and division

--- public/calculator.js
+++ public/calculator.js
@@ -2,13 +2,41 @@ function sum(a, b) {
     return a + b
 }
 
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
 // "a" + "b" => "ab"
 // 1 + 1 => 2
 
-const button = document.getElementById("button-sum")
+const buttonSum = document.getElementById("button-sum")
+const buttonSubtract = document.getElementById("button-subtract")
+const buttonMultiply = document.getElementById("button-multiply")
+const buttonDivide = document.getElementById("button-divide")
+
+buttonSum.addEventListener("click", function(event) {
+    const inputA = document.getElementById("input-a")
+    const inputB = document.getElementById("input-b")   
+    const inputResult = document.getElementById("input-result")
 
 
-button.addEventListener("click", function(event) {
+    // int => Integer = Number = 1, 1000, 1000000
+    const valueA = parseInt(inputA.value)
+    const valueB = parseInt(inputB.value)
+
+    const result = sum(valueA, valueB)
+
+    inputResult.value = result
+})
+
+buttonSubtract.addEventListener("click", function(event) {
     const inputA = document.getElementById("input-a")
     const inputB = document.getElementById("input-b")
     const inputResult = document.getElementById("input-result")
@@ -18,7 +46,38 @@ button.addEventListener("click", function(event) {
     const valueA = parseInt(inputA.value)
     const valueB = parseInt(inputB.value)
 
-    const result = sum(valueA, valueB)
+    const result = subtract(valueA, valueB)
+
+    inputResult.value = result
+})
+
+
+buttonMultiply.addEventListener("click", function(event) {
+    const inputA = document.getElementById("input-a")
+    const inputB = document.getElementById("input-b")
+    const inputResult = document.getElementById("input-result")
+
+
+    // int => Integer = Number = 1, 1000, 1000000
+    const valueA = parseInt(inputA.value)
+    const valueB = parseInt(inputB.value)
+
+    const result = multiply(valueA, valueB)
+
+    inputResult.value = result
+})
+
+buttonDivide.addEventListener("click", function(event) {
+    const inputA = document.getElementById("input-a")
+    const inputB = document.getElementById("input-b")
+    const inputResult = document.getElementById("input-result")
+
+
+    // int => Integer = Number = 1, 1000, 1000000
+    const valueA = parseInt(inputA.value)
+    const valueB = parseInt(inputB.value)
+
+    const result = divide(valueA, valueB)
 
     inputResult.value = result
 })
