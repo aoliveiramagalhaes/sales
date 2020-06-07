4b33c63484d1d826bbf1008ef7ec8fbb071ef9a4
Pablo Cantero <pablohstc@gmail.com>
2020-06-07 17:45:56 -0400

    Add quiz 1

---
+++ quiz1.spec.js
@@ -0,0 +1,23 @@
+it("quiz 1", () => {
+    let fruits = ['orange', 'apple', 'mango']
+    // ...
+    expect(fruits).toBe(['orange', 'kiwi'])
+});
+
+it("quiz 2", () => {
+    let fruits = ['orange', 'apple', 'mango']
+    // ...
+    expect(fruits).toBe(['orange', 'apple'])
+});
+
+it("quiz 3", () => {
+    let msg = "Hello"
+    // ...
+    expect(msg).toBe("Hello, Aline")
+});
+
+it("quiz 4", () => {
+    let count = 0
+    // ...
+    expect(count).toBe(10)
+});
