c9df08e726156423ed4fac3a0465fae7a687acb0
Aline <aoliveiramagalhaes@gmail.com>
2020-06-08 14:39:53 -0400

    replied to quiz

--- quiz1.spec.js
+++ quiz1.spec.js
@@ -1,39 +1,23 @@
-// yarn jest quiz1.spec.js
 it("quiz 1", () => {
-    let fruits = ["orange", "apple", "mango"]
-    fruits.splice(1, 2, "kiwi");
-
+    let fruits = ['orange', 'apple', 'mango']
+    fruits.splice(1, 2, 'kiwi');
     expect(fruits).toEqual(['orange', 'kiwi'])
 });
 
 it("quiz 2", () => {
-    let fruits = ["orange", "apple", "mango"]
-    fruits.pop();
+    let fruits = ['orange', 'apple', 'mango']
+    fruits.pop()
     expect(fruits).toEqual(['orange', 'apple'])
 });
 
 it("quiz 3", () => {
     let msg = "Hello"
-    msg += ", Aline"
+    msg = msg + (", Aline")
     expect(msg).toEqual("Hello, Aline")
-});
-
-it("quiz 3.1", () => {
-    let msg = "Hello"
-    msg += ", Aline, Pablo, Mia"
-    expect(msg).toEqual("Hello, Aline, Pablo, Mia")
 });
 
 it("quiz 4", () => {
     let count = 0
-    count += 10
     count = count + 10
-    expect(count).toEqual(20)
-});
-
-it("quiz 5", () => {
-    let count = 3
-    // count = count * 3
-    count *= 3
-    expect(count).toEqual(9)
+    expect(count).toEqual(10)
 });
